f97feda07f37ef7a99d6cc44096d4fc9ff2c99bc
Curtis Bowman <c.bowman1711@gmail.com>
2023-03-24 02:26:17 -0400

    new aliases

---
+++ custom/aliases.zsh
@@ -0,0 +1,11 @@
+#!/usr/bin/zsh
+
+
+alias la='ls -a'
+alias ll='ls -la'
+
+alias em='emacsclient -nw'
+alias emacs='emacsclient -nc &'
+
+alias alias='emacsclient -nw $ZSH_CUSTOM/aliases.zsh'
+alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
